8cde65b831d0ba34d059a1f80ec7e413b866c67d
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-05-08 06:53:51 +0530

    basic server set up, server running on port:5000

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port http://localhost:${port}`)
+})
