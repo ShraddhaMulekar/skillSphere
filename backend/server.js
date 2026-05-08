36e73c4fce1be3182ae75fde6ebb2ba9aa3cc917
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-05-08 07:08:09 +0530

    server file updated

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
 import express from "express"
 import cors from "cors"
 import dotenv from 'dotenv'
+import helmet from "helmet"
 import { connectDB } from "./config/mongoDB.js"
+
+// Load env variables
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 8080
 
+// Middlewares
+app.use(helmet())
 app.use(express.json())
 app.use(cors())
 
-app.listen(port, ()=>{
-    connectDB()
+// Start server
+app.listen(port, async ()=>{
+    await connectDB()
     console.log(`Server is running on port http://localhost:${port}`)
 })
