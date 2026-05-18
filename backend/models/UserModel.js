0daca19f41eafdb616b4f85932b320cc060fe657
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-05-18 15:26:47 +0530

    add user model with schema definition and validation

---
+++ backend/models/UserModel.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [6, "Password must be at least 6 characters long"],
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["client", "freelancer", "admin"],
+      default: "client",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    avator: {
+      type: String,
+      default: "",
+    },
+    emailVerificationToken: String,
+    passwordResetToken: String,
+    passwordResetExpire: Date,
+    twoFactorEnabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const UserModel = mongoose.model("user", userSchema);
